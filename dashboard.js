31311b0ee0cd0452e629c04d9c81981f3f641f70
puneetyadav00 <149051239+puneetyadav00@users.noreply.github.com>
2026-07-12 08:25:01 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,35 @@
+import { db } from "./firebase-config.js";
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
+async function loadTeams(){
+
+const snapshot = await getDocs(collection(db,"registrations"));
+
+let html = "<h2 style='color:red'>Registered Teams</h2>";
+
+snapshot.forEach((doc)=>{
+
+const t = doc.data();
+
+html += `
+<div style="background:#111;border:2px solid red;padding:15px;margin:15px 0;border-radius:10px;">
+<b>IGL:</b> ${t.igl}<br>
+<b>Player 1:</b> ${t.player1}<br>
+<b>Player 2:</b> ${t.player2}<br>
+<b>Player 3:</b> ${t.player3}<br>
+<b>Player 4:</b> ${t.player4}<br>
+<b>WhatsApp:</b> ${t.whatsapp}<br>
+<b>UTR:</b> ${t.payment}
+</div>
+`;
+
+});
+
+document.body.innerHTML += html;
+
+}
+
+loadTeams();
