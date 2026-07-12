572a693f307a1c72728b794fd36fdcfbd55e1109
puneetyadav00 <149051239+puneetyadav00@users.noreply.github.com>
2026-07-12 08:45:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,29 @@ setTimeout(()=>{
 alert("🔥 Welcome to ARMS RIDERS ESPORTS 🔥");
 
 },3500);
+const targetDate = new Date("December 31, 2026 23:59:59").getTime();
+
+setInterval(()=>{
+
+const now = new Date().getTime();
+
+const distance = targetDate-now;
+
+const days=Math.floor(distance/(1000*60*60*24));
+
+const hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+const minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+
+const seconds=Math.floor((distance%(1000*60))/1000);
+
+const timer=document.getElementById("countdown");
+
+if(timer){
+
+timer.innerHTML=
+"🏆 Next Championship Starts In<br>"+days+"d : "+hours+"h : "+minutes+"m : "+seconds+"s";
+
+}
+
+},1000);
